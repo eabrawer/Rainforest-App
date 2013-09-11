f423227c6ff7b69e0334e5610bb1f2e5b6541529
Eric Brawer <ericbrawer@gmail.com>
2013-09-11 14:56:50 -0400

    Refactoring the products controller to respond to JSON

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,10 +2,18 @@ class ProductsController < ApplicationController
 
   def index
   	@products = Product.all
+  	respond_to do |format|
+  		format.html
+  		format.json { render json: @products }
+  	end
   end
 
   def show
   	@product = Product.find(params[:id])
+  	respond_to do |format|
+  		format.html
+  		format.json { render json: @product }
+  	end  	
   end
 
   def edit
@@ -14,35 +22,49 @@ class ProductsController < ApplicationController
 
   def update
   	 @product = Product.find(params[:id])
-  	 if @product.update_attributes(product_params)
-  	 	redirect_to product_path
-  	 else 
-  	 	render :edit
-  	 end
+  	 respond_to do |format|
+	  	 if @product.update_attributes(product_params)
+	  	 	format.html {redirect_to product_path notice: "Product was succesfully updated."}
+	  	 	format.json { head :no_content }
+	  	 else 
+	  	 	format.html { render action: "edit" }
+	        format.json { render json: @product.errors, status: :unprocessable_entity }
+  	 	end
+  	end
   end
 
   def new
   	@product = Product.new
+  	respond_to do |format|
+  		format.html
+  		format.json { render json: @product }
+  	end  	
   end
 
   def create 
   	@product = Product.new(product_params)
-  	if @product.save
-  		redirect_to products_url
-    else
-      	render :new
-    end
+  	respond_to do |format|
+	  	if @product.save
+	  		format.html {redirect_to @product, notice: "Product was succesfully created"}
+	  		format.json {render json: @product , status: :created, location: @product}
+	    else
+	       format.html { render action: "new" }
+       		format.json { render json: @product.errors, status: :unprocessable_entity }
+	    end
+	end
   end 
 
   def destroy
 	@product = Product.find(params[:id])
 	@product.destroy
-	redirect_to products_url
+		respond_to do |format|
+      		format.html { redirect_to products_url }
+      		format.json { head :no_content }
+      	end
   end
 
   def product_params
   	params.require(:product).permit(:name, :description, :price_in_cents)
   end
-
 
 end
